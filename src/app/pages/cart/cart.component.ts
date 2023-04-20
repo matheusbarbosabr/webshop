1b8133ae927877cc91877d8113704d635baa6dd2
Matheus Barbosa <matheusbarbosasilva.br@gmail.com>
2023-04-20 14:27:02 -0300

    feat: Implements payment through stripe

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,6 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-cart',
@@ -32,7 +36,7 @@ export class CartComponent {
     'action'
   ];
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.cartService.cart.subscribe((_cart: Cart) => {
@@ -59,5 +63,16 @@ export class CartComponent {
 
   onRemoveQuantity(item: CartItem): void {
     this.cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void {
+    this.http.post('http://localhost:4242/checkout', {
+      items: this.cart.items
+    }).subscribe(async(res: any) => {
+      let stripe = await loadStripe(environment.API_PUBLISHABLE_KEY);
+      stripe?.redirectToCheckout({
+        sessionId: res.id
+      })
+    })
   }
 }
